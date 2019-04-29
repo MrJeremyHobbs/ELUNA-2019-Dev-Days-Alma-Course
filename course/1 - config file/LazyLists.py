101a41925a1f8eeef1c9973d03a5502b963564d9
Jeremy Hobbs <mr.hobbsy@gmail.com>
2019-04-29 11:42:30 -0400

    fixed lesson 1

--- course/1 - config file/LazyLists.py
+++ course/1 - config file/LazyLists.py
@@ -1,16 +1,7 @@
 #!/usr/bin/python3
-import configparser
 
 # configurations ##############################################################
-config = configparser.ConfigParser()
-config.read('config.ini')
 
-apikey = config['misc']['apikey']
-set_id = config['misc']['set_id']
-operating_system = config['misc']['os']
-author = config['misc']['author']
-
-print(author)
 
 # main program ################################################################
 
